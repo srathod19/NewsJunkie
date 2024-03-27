50bd4c4c45312566b5fee63dc7552c9808585551
Sachin R <sachinrathod@hyperlinkinfosystem.net.in>
2024-03-27 19:55:46 +0530

    updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,13 +8,13 @@ export class NewsItem extends Component {
                 <div className="card">
                     <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+                        <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
                             {source.name}
-                            <span class="visually-hidden">unread messages</span>
+                            <span className="visually-hidden">unread messages</span>
                         </span>
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text" >{description}</p>
-                        <p className="card-text"><small class="text-body-secondary">By {author ? author : "unknown"} on {new Date(date).toGMTString()} </small></p>
+                        <p className="card-text"><small className="text-body-secondary">By {author ? author : "unknown"} on {new Date(date).toGMTString()} </small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More..</a>
                     </div>
                 </div>
